7f7b70b856bfaec7e3921697b914ffbc1439f982
ygorkagrishin <webygorkagrishin@gmail.com>
2018-10-05 21:05:51 +0300

    Добавлена страница contact-page

--- assets/static/js/map-api.js
+++ assets/static/js/map-api.js
@@ -1,1 +1,13 @@
-console.log('map-api init');
+// ymaps.ready(function () {
+
+//   const ymap = document.querySelector('#map');
+
+//   if (!ymap)
+//     return;
+
+//   const map = new ymaps.Map(ymap, {
+//     center: [55.76, 37.64],
+//     controls: [],
+//     zoom: 10
+//   });
+// });
