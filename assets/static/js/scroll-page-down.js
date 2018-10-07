a27c33276876e5e1981b8183c14857a7b600f29f
ygorkagrishin <webygorkagrishin@gmail.com>
2018-10-07 18:47:11 +0300

    bugfix ie11

--- assets/static/js/scroll-page-down.js
+++ assets/static/js/scroll-page-down.js
@@ -1,31 +1,37 @@
-document.body.querySelector('.js-page-wrapper').addEventListener('click', function (e) {
+(function () {
+
+const trigger = document.body.querySelector('#js-scroll-down');
+
+return trigger !== (undefined || null) ? trigger.addEventListener('click', function (e) {
   e.preventDefault();
-  
+    
   const viewPort = window;
   const viewPortHeight = viewPort.innerHeight;
-  
+    
   const scrolled = viewPort.pageYOffset;  
   const scrollTo = viewPortHeight - scrolled;
-
+    
   const wrapper = document.body.querySelector('.js-page-wrapper');
-
+    
   const duration = 700;
-
+    
   const styleProperety = wrapper.style;
-
+    
   if (!wrapper.classList.contains('active')) {
     wrapper.classList.add('active');
-
+    
     styleProperety.transform = `translateY(-${scrollTo}px)`;
     styleProperety.transition = `${duration}ms`;
   } else return;
-
+    
   setTimeout(() => {
     if (wrapper.classList.contains('active')) 
-      wrapper.classList.remove('active');
-
+    wrapper.classList.remove('active');
+    
     wrapper.removeAttribute('style');
     viewPort.scrollTo(0, viewPortHeight);
-
+    
   }, duration);
-});
+}) : false;
+
+})()
